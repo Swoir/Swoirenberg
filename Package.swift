be0ad85186858e78bf6ac8e77be2288514bd2095
Theo Madzou <madztheo@hotmail.fr>
2025-01-02 16:55:48 +0000

    upgrade to Noir 1.0.0-beta.1

--- Package.swift
+++ Package.swift
@@ -21,8 +21,8 @@ let package = Package(
             linkerSettings: [ .linkedFramework("SystemConfiguration") ]),
         .binaryTarget(
             name: "SwoirenbergFramework",
-            url: "https://github.com/Swoir/Swoirenberg/releases/download/v1.0.0-beta.0-1/Swoirenberg.xcframework.zip",
-            checksum: "d12ea1b12cfc5d4af0116741b2242a06cf6c8ef77e853d92111150d4a803e7fb"),
+            url: "https://github.com/Swoir/Swoirenberg/releases/download/v1.0.0-beta.1-1/Swoirenberg.xcframework.zip",
+            checksum: "84a1f524a5d2521443c68069c517ae53dd387d2c8f60a6bf0df8f3d7c99f791c"),
         .testTarget(
             name: "SwoirenbergTests",
             dependencies: ["Swoirenberg"],
